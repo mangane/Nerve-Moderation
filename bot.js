5294222e9a76786c71e435bc095dc1d3408a15e8
mangane <40431348+mangane@users.noreply.github.com>
2019-07-26 04:23:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,5 +14,34 @@ client.on('ready', () => {
             client.user.setPresence({ game: { name: `${client.users.size} Utilisateurs `, type: "WATCHING" } });
         }, 1*70000);
 });
+client.on ("message", async message => {
+ if (command === "ban") {
+		 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandée");	
 
+    var amsg = message.content;
+		// supprime le premier mot de la chaine (string) donc "!pub machin" va supprimer pub
+		var msg = amsg.substr(amsg.indexOf(" ") + 23);
+   var reason = msg;
+    if (!reason || reason < 250 ) return message.channel.send("***Vous n'avez pas mis de raison ou vous en avez mise une trop longue !!***")
+
+    let member = message.mentions.members.first ();
+    let member2 = message.mentions.users.first ();
+   var embed = new Discord.RichEmbed()
+   .setColor ("#f08619")
+   .setTitle ("Vous venez de vous faire bannir :warning:")
+   .addField("Par le modérateur/administrateur :", message.author.username + "#" + message.author.discriminator)
+   .addField("Depuis le serveur :", message.guild.name)
+   .addField ("Pour la raison suivante :", reason)
+   .setFooter ("Vous êtes bannis, vous ne pourrez y retournez que en cas de unban 💮 !!")
+    
+    let search = message.guild.channels.find(`name`, "modlogs")
+    const a = new Discord.RichEmbed()
+    .setTitle("test")
+    member.send ({embed})
+   
+message.channel.send ("```diff\nL'utilisateurs "+member2.username +"#"+ member2.discriminator +"\ Viens de ce faire kick pour la raison ci-dessous :\n``` ``\n\n"+ reason + " 🔨 ``")
+ member.ban();
+     
+  }
+});
 client.login(process.env.BOT_TOKEN)
