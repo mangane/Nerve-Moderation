0a4b5ca74a7da9dfb4d6c4214f98ba8c4f8cf405
mangane <40431348+mangane@users.noreply.github.com>
2019-07-26 04:09:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+const Discord = require('discord.js');
+let client = new Discord.Client();
+var prefix = "!";
+var prefix2 = "!";
 
+client.on('ready', () => {
+	setInterval(() => {
+            client.user.setPresence({ game: { name: `Nerve Moderation `, type: "WATCHING" } });
+        }, 1*30000);
+	setInterval(() => {
+            client.user.setPresence({ game: { name: `By Gaétan`, type: "PLAYING" } });
+        }, 1*40000);
+	setInterval(() => {
+            client.user.setPresence({ game: { name: `${client.users.size} Utilisateurs `, type: "WATCHING" } });
+        }, 1*70000);
+});
+
+client.login(process.env.BOT_TOKEN)
